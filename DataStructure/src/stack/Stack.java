a4088d62fc3e2fc83d5f099ac10f17d8965c90ef
Dohyun Park <freesin94@gmail.com>
2019-02-24 15:29:52 +0900

    feat : peek()

--- DataStructure/src/stack/Stack.java
+++ DataStructure/src/stack/Stack.java
@@ -1,7 +1,39 @@
 package stack;
 
 public class Stack {
+	int[] stackArray; 
+	int top;
+	int size;
 	Stack(){
+		
+	}
+	
+	Stack(int s){
+		size = s;
+		stackArray = new int[size];
+		top = 0;
+	}
+	
+	public int peek() {
+		if(isEmpty()) {
+			System.out.println("stack is empty");
+			return 0;
+			
+		}
+		else if(isFull()) {
+			System.out.println("stack is full");
+			
+		}
+		return stackArray[top];
+		
+	}
+	
+	public boolean isFull() {
+		return (top == size-1);
+	}
+	
+	public boolean isEmpty() {
+		return (top == 0);
 		
 	}
 }
