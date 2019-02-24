e8b6fd57004d4b7d125601ccf4a061cbd4e34312
cover <cover@gimgwanhoui-mbp>
2019-02-24 15:08:15 +0900

    feat: Add isFull()

--- DataStructure/src/stack/Stack.java
+++ DataStructure/src/stack/Stack.java
@@ -1,7 +1,19 @@
 package stack;
 
 public class Stack {
-	Stack(){
+	
+	// 가장 위에 있는 stack
+	int top, maxSize;
+	
+	Stack() {
 		
+	}
+
+	public boolean isFull() {
+		// 스택이 꽉 찬 경우, 가장 위에 있는 스택이 멕시멈보다 1작은 경우가 꽉찬 경
+		if(top == maxSize -1) {
+			return true;
+		}else
+			return false;
 	}
 }
