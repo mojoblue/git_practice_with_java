d1164db836bdd15e11210d36238f5c93f6aed06f
zzoyeon9 <zzoyeon9@naver.com>
2019-02-24 15:31:49 +0900

    feat: Add isEmpty() & constructor on Stack class

--- DataStructure/src/stack/Stack.java
+++ DataStructure/src/stack/Stack.java
@@ -1,17 +1,18 @@
 package stack;
 
 public class Stack {
-		int head=0;
-	
+		int top=0;
+	Stack(){
+		int [] stackArray = new int[10];
+		stackArray[0]=top;
+		top = stackArray[0];
+	}
 	Stack(int size){
-		
-		if(size==0)
-			int [] stackArray = new int[10];
-		else
+	
 			int [] stackArray = new int[size];
 		
-		stackArray[0]=head;
-		head = stackArray[0];
+		stackArray[0]=top;
+		top = stackArray[0];
 	}
 	
 	public boolean isEmpty() {
