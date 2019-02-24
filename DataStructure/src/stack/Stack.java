0ec8e9f57d8875aee23fd0e2c6ab72d441450466
cover <cover@gimgwanhoui-mbp>
2019-02-24 16:41:11 +0900

    feat : Make int pop()

--- DataStructure/src/stack/Stack.java
+++ DataStructure/src/stack/Stack.java
@@ -2,18 +2,16 @@ package stack;
 
 public class Stack {
 	
-	// 가장 위에 있는 stack
-	int top, maxSize;
-	
 	Stack() {
 		
 	}
 
-	public boolean isFull() {
-		// 스택이 꽉 찬 경우, 가장 위에 있는 스택이 멕시멈보다 1작은 경우가 꽉찬 경
-		if(top == maxSize -1) {
-			return true;
-		}else
-			return false;
+	public int pop() {
+		if(!isEmpty()) {
+			return Stack[top--];
+		}
+		// data가 없을 때, null
+		else return null;		
 	}
 }
+
