e614bb53f7ee35a0b3024fd9d620ab7c24548c79
zzoyeon9 <zzoyeon9@naver.com>
2019-02-24 15:16:39 +0900

    feat:isEmpty() & contructor on Stack class

--- DataStructure/src/stack/Stack.java
+++ DataStructure/src/stack/Stack.java
@@ -1,7 +1,24 @@
 package stack;
 
 public class Stack {
+	int top = 0;
+	
 	Stack(){
+		int [] stackArray = new int[10];
+		stackArray[0]=head;
+		head = stackArray[0];
+	}
+	Stack(int size){
 		
+			int [] stackArray = new int[size];
+		
+		stackArray[0]=head;
+		head = stackArray[0];
+	}
+	
+	public boolean isEmpty() {
+		if(head == null)
+			System.out.println("stack is empty");
+		return true;
 	}
 }
